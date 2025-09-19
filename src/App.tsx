7eed81a3058cc866ec6ad1afe8044e009868832a
jaejae581 <rlarudvlf456@gmail.com>
2025-09-19 16:36:19 +0900

    Initial commit: Setup React project with Tailwind CSS

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="p-10">
+      <h1 className="text-3xl font-bold underline text-purple-600">
+        추첨 프로그램 만들기!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
